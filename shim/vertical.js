76caca3b4f100f4555ad49e2c3ebd80631716eb7
dumorando <144301348+dumorando@users.noreply.github.com>
2024-05-31 13:17:00 -0700

    work?

--- shim/vertical.js
+++ shim/vertical.js
@@ -1,1 +1,1 @@
-module.exports = require('imports-loader?Blockly=./blocks_compressed_vertical-blockly_compressed_vertical-messages,goog=./blockly_compressed_vertical.goog!exports-loader?Blockly!../msg/scratch_msgs');
+module.exports = require('imports-loader?Blockly=./blocks_compressed_vertical-blockly_compressed_vertical-messages.js,goog=./blockly_compressed_vertical.goog!exports-loader?Blockly!../msg/scratch_msgs.js');
